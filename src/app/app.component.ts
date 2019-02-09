fce235a18e07439511dab4c9eaf2629138ed6d9a
ranwahle <ran.wahle@gmail.com>
2019-02-10 01:19:17 +0200

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Task} from './model/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  tasks: Task[] = [];
   title = 'forescout-angular-workshop';
+
+  constructor() {
+    this.initTasks();
+
+  }
+
+  initTasks() {
+    for (let i = 0; i < 10; i++) {
+      this.tasks.push({title: `Task ${i + 1} title`, done: false, description: `Task ${i + 1} Description`});
+    }
+  }
 }
