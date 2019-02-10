9651a7a40e1d25df1f1742d87bf8205ed6f97397
ranwahle <ran.wahle@gmail.com>
2019-02-10 02:24:26 +0200

    Final commit

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,3 +1,8 @@
 import {Routes} from '@angular/router';
+import {TasksListComponent} from './tasks-list/tasks-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '', redirectTo: '/tasksList', pathMatch: 'full'
+}, {
+  path: 'tasksList', component: TasksListComponent
+}];
